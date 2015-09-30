da19c308befbc6c95a8c379dc0a0c8ffeba8b546
Ivar <Ivar@158.38.192.151>
2015-09-30 12:59:25 +0200

    FUCK YEAH!

--- SanntidVideo/src/sanntidvideo/Midi.java
+++ SanntidVideo/src/sanntidvideo/Midi.java
@@ -1,10 +1,46 @@
-
 package sanntidvideo;
+
+import java.util.ArrayList;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiUnavailableException;
 
 /**
  *
  * @author Ivar
  */
 public class Midi {
-    
+
+    int channel = 1;
+
+    private static MidiChannel[] channels;
+
+    static {
+        try {
+            Synthesizer synth = MidiSystem.getSynthesizer();
+            synth.open();
+            channels = synth.getChannels();
+        } catch (MidiUnavailableException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void StartNote(ArrayList<Integer> midiCode, ArrayList<Integer> volume) {
+        for (int i : midiCode) {
+            channels[channel].noteOn(midiCode.get(i), volume.get(i));
+        }
+    }
+
+    public void EndNote(ArrayList<Integer> midiCode) {
+        for (int i : midiCode) {
+            channels[channel].noteOff(midiCode.get(i));
+        }
+    }
+
+    public void EndAllNotes() {
+        channels[channel].allNotesOff();
+    }
+
 }
