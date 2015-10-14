56697e37b23fbf8049cdd3e4059b236969ea078d
Christian <Christian@158.38.192.116>
2015-10-14 15:51:41 +0200

    Merge origin/master
    Conflicts:
    	SanntidVideo/src/sanntidvideo/Kommunikasjon.java

--- SanntidVideo/src/sanntidvideo/Kommunikasjon.java
+++ SanntidVideo/src/sanntidvideo/Kommunikasjon.java
@@ -5,29 +5,21 @@
  */
 package sanntidvideo;
 
-import static com.sun.glass.ui.Application.run;
 import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author Christian
  */
-public class Kommunikasjon extends Thread {
+public class Kommunikasjon implements Runnable {
 
     private ServerSocket server;
     private Socket sock;
@@ -38,46 +30,32 @@ public class Kommunikasjon extends Thread {
     BufferedImage img;
     InputStream input;
     OutputStream os;
+    byte[] byteimg;
 
     public Kommunikasjon() {
         cap = new VideoCap();
-
-//        InputStreamReader IR = new InputStreamReader(sock.getInputStream());
-//        BufferedReader BR = new BufferedReader(IR);
-//        String Message = BR.readLine();
-//        System.out.println(Message);
-//        if (Message != null) {
-//            PrintStream PS = new PrintStream(sock.getOutputStream());
-//            PS.println("Message Recived");
-//        }
     }
 
     public void run() {
         try {
             server = new ServerSocket(25000);
             System.out.println("Server running: " + server.isBound());
-            sock = server.accept();
-            img = cap.getOneFrame();
-            os = sock.getOutputStream();
-            
-            
-            
-//                ImageIO.write(img, "jpg", sock.getOutputStream());
-            //sock.close();
+            int i=0;
+            while (true) {
+                sock = server.accept();
+                os = sock.getOutputStream();
+                img = cap.getOneFrame();
+                baos = new ByteArrayOutputStream();
+
+                ImageIO.write(img, "jpg", baos);
+                byte[] size = ByteBuffer.allocate(4).putInt(baos.size()).array();
+                os.write(size);
+                System.out.println(baos.size());
+                os.write(baos.toByteArray());
+                os.flush();
+            }
         } catch (IOException e) {
         }
         System.out.println("Server failed");
-    }
-
-    public void sendInt() {
-
-    }
-
-    public void recievePicture() {
-
-    }
-
-    public void recieveBool() {
-
     }
 }
