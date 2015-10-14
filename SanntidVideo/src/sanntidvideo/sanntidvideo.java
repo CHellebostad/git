576dd33fa9029c887ab25be879774207925b8fa4
Ivar <Ivar@158.38.192.132>
2015-10-14 16:53:33 +0200

    FUCK YEAH!

--- SanntidVideo/src/sanntidvideo/sanntidvideo.java
+++ SanntidVideo/src/sanntidvideo/sanntidvideo.java
@@ -5,24 +5,50 @@
  */
 package sanntidvideo;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.opencv.core.Core;
 
 /**
  *
  * @author Christian
  */
-public class sanntidvideo{
+public class sanntidvideo {
 
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
+    static BildeSplit bs = new BildeSplit();
+    static VideoCap vc = new VideoCap();
+    static ArrayList<BufferedImage> bilderTilAnalyse = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException {
-        new Thread(new Kommunikasjon()).start();
+    public static void main(String[] args) throws IOException, NullPointerException {
+        //new Thread(new Kommunikasjon()).start();
+
+        //BufferedImage bildeTilSplitting = vc.getOneFrame();
+        File bildeTilSplitting = new File("img3.jpg");
         
-        
+        bilderTilAnalyse = bs.Split(bildeTilSplitting);
+
+            
+            Thread t0 = new Thread(new Analyse(bilderTilAnalyse.get(0)));
+            t0.start();
+            Thread t1 = new Thread(new Analyse(bilderTilAnalyse.get(1)));
+            t1.start();
+            Thread t2 = new Thread(new Analyse(bilderTilAnalyse.get(2)));
+            t2.start();
+            Thread t3 = new Thread(new Analyse(bilderTilAnalyse.get(3)));
+            t3.start();
+            Thread t4 = new Thread(new Analyse(bilderTilAnalyse.get(4)));
+            t4.start();
+            Thread t5 = new Thread(new Analyse(bilderTilAnalyse.get(5)));
+            t5.start();
         
 
     }
+
+    
+
 }
